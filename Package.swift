a199a7c8055111fb9f2bd2c19ed8551d64005db6
Paulo Faria <paulo.faria.rl@gmail.com>
2016-02-29 10:17:53 -0300

    bump to version 0.3

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/Zewo/File.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 3),
         .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 2)
     ]
