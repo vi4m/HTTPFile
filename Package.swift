d29f231685ce78b5388eccd640464c7e2c18d939
✪ vi4m Marcin Kliks <vi4m@users.noreply.github.com>
2016-05-26 10:43:10 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "HTTPFile",
     dependencies: [
-        .Package(url: "https://github.com/VeniceX/File.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/VeniceX/File.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 5),
     ]
 )
